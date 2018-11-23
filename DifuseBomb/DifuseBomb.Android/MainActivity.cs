2132ab3fe06e88a5c34738ab6cce0892393a6d6a
Amruta Choure <amruta070712@gmail.com>
2018-11-23 22:30:17 +0800

    running app

--- DifuseBomb/DifuseBomb.Android/MainActivity.cs
+++ DifuseBomb/DifuseBomb.Android/MainActivity.cs
@@ -1,2 +1,21 @@
-﻿
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+
+namespace DifuseBomb.Droid
+{
+    [Activity(Label = "DifuseBomb", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+		
+
+	
+	}
+}
 
